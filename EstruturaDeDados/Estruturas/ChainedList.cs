4650c1592960f451bb0f7d6938558a3432ba5b7b
Carlos <carloseduardo2101@gmail.com>
2022-02-22 14:57:29 -0300

    Started making the Chained List class

---
+++ EstruturaDeDados/Estruturas/ChainedList.cs
@@ -0,0 +1,31 @@
+﻿namespace Estruturas
+{
+    public class ChainedList
+    {
+        private Node root;
+        public int Size { get; private set; }
+
+        #region Constructors
+
+        public ChainedList()
+        {
+            root = null;
+            Size = 0;
+        }
+
+        public ChainedList(object obj)
+        {
+            if (obj.GetType() == typeof(Node))
+                root = (Node) obj;
+            else
+                root = new Node(obj);
+        }
+
+        #endregion
+
+        public void Add()
+        {
+            
+        }
+    }
+}
